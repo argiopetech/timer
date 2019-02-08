54831ccebff409ac2ee11b970c4963a9737756c1
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-02-07 21:07:09 -0500

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

---
+++ src/Statistics/Distribution/EmpiricalDistribution.hs
@@ -0,0 +1,38 @@
+module Statistics.Distribution.EmpiricalDistribution where
+
+import File
+
+import Data.List     (sort, partition)
+import Data.Time.Clock
+import Statistics.Distribution (Distribution, cumulative)
+
+
+newtype EmpiricalDistribution = EmpiricalDistribution (NominalDiffTime -> Double)
+
+instance Distribution EmpiricalDistribution where
+  cumulative (EmpiricalDistribution f) = f . realToFrac
+
+dataToPercentile :: FileFormat -> [(Int, EmpiricalDistribution)]
+dataToPercentile f =
+  let (ls, lData) = unzip $ onlyValidSplits $ levelData f
+      fs = map toF lData
+  in zip ls fs
+  where toF :: [NominalDiffTime] -> EmpiricalDistribution
+        toF ls =
+          let slData = sort ls
+              tLen   = fromIntegral $ length slData
+              f = \t -> let p = partition (<= t) slData
+                        in case p of
+                             ([], []) -> 0.0
+                             ([], _ ) -> 0.0
+                             (_,  []) -> 1.0
+                             (l,  m ) ->
+                               let lBound = last l
+                                   uBound = head m
+                                   tFrac  = realToFrac (t - lBound) / realToFrac (uBound - lBound)
+                                   lLen   = fromIntegral $ length l
+                                   lPctl  = lLen / tLen
+                                   uPctl  = (lLen + 1) / tLen
+                                   delta  = uPctl - lPctl
+                               in lPctl + tFrac * delta
+          in EmpiricalDistribution f
