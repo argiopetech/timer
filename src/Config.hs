fd0422397e2d9b2e00e852a75117e129718f8d10
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-02-07 23:39:08 -0500

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- src/Config.hs
+++ src/Config.hs
@@ -12,20 +12,12 @@ import Data.Yaml.Pretty hiding (Config)
 data Config = Config Text [Level]
             deriving (Show)
 
-instance ToJSON Config where
-  toJSON (Config t ls) = object [ "title"  .= t
-                                , "levels" .= ls ]
-
 instance FromJSON Config where
   parseJSON (Object o) = Config <$> o .: "title" <*> o .: "levels"
 
 data Level = Level { levelName :: Text
                    , best      :: NominalDiffTime
                    } deriving (Show)
-
-instance ToJSON Level where
-  toJSON (Level n tb) = object [ "name"       .= n
-                               , "time"       .= object ["best" .= tb]]
 
 instance FromJSON Level where
   parseJSON (Object l) = do
