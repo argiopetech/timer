6690e362ab7d22a9e959ea11dacc8f489138d1c4
Elliot Robinson <elliot.robinson@argiopetech.com>
2019-02-08 12:43:57 -0500

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- distr-test/Main.hs
+++ distr-test/Main.hs
@@ -2,12 +2,11 @@ module Main where
 
 import Config
 import File
-import Format.SplitTime
 
 import Statistics.Distribution
 import Statistics.Distribution.Empirical
 
-import Control.Monad.IO.Class (liftIO)
+import Data.List (sort)
 import Data.Yaml
 
 import Text.Printf
@@ -16,17 +15,17 @@ import Text.Printf
 main :: IO ()
 main = do
   -- YAML setup
-  yml <- liftIO $ decodeFileEither "splits.yaml"
+  yml <- decodeFileEither "splits.yaml"
 
   f <- case yml of
          Right (Config _ levels) -> load levels
          Left  _ -> load'
 
   let distrs = levelEmpiricalDistribution f
-      l1Data = snd $ head $ onlyValidSplits $ levelData f
-      m      = realToFrac $ minimum l1Data
-      ma     = realToFrac $ maximum l1Data
+      l1Data = map realToFrac $ sort $ snd $ head $ onlyValidSplits $ levelData f
+      m      = minimum l1Data
+      ma     = maximum l1Data
       domain = [m, m+0.001..ma]
-      range  = map (cumulative (snd $ head distrs)) domain
+      range  = map (cumulative (snd $ head distrs)) l1Data -- domain
 
   mapM_ putStrLn $ map (uncurry $ printf "%f %f") $ zip domain range
